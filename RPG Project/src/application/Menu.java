ed75c957c215049b6b05dddafe1e8de58ee93c51
phncosta <phncosta@outlook.com>
2019-09-28 16:29:10 -0300

    Reformatacao do Menu

--- RPG Project/src/application/Menu.java
+++ RPG Project/src/application/Menu.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class Menu {
 
 	public static void main(String[] args) {
-		// Menu Base - Exemplo ~ Protótipo
+		// Menu Base - Exemplo ~ Protótipo v1.3
 
+		/* //////  IMPORTANTE!  //////
+		 * O caracter Unicode UTF-8 da sua IDE (Eclipse ou NetBeans) deve estar ativo/permitido para mostrar os desenhos em forma de caracteres desse jogo.
+		-> No Eclipse, acesse: Window > Preferences > General > Workspace > Text File Encoding mude Other: para Unicode UTF-8.
+		~~ Feito isso, feche e abra o Eclipse novamente.
+		No NetBeans, senta e chora. Desinstale essa droga e use uma IDE de verdade.
+		*/
+		
 		Scanner input = new Scanner(System.in);
 
 		boolean sair = false;
 		boolean voltar = true;
-		String opcao, opcaoCondicional;
-		String sinopse = "SINOPSE: \n( - A ser preenchido )\n\nDesenvolvido e projetado por: \n - Caio Vinicius \n - João Vitor \n - Paulo Costa \n - Renan Scarpato \n\nTodos os direitos reservados. ©";
-
+		String opcao, opcaoCondicional = null, opcaoInvalida = "Opção inválida.\nVoltar? (s/n)";
+		String sobre = "SINOPSE: \n(A ser preenchido)\n\nDesenvolvido e projetado por: \n - Caio Vinicius \n - João Vitor \n - Paulo Costa \n - Renan Scarpato \n"
+				+ "\nLinguagem desenvolvida: Java\nMatéria: Conceitos de Computação\n\n© Todos os direitos reservados.\n"; // temp = temporário
+		String opcoes = "Você acessou as opções."; // temp
+		String jogar = "Você acessou jogar."; // temp
+		
 		do {
 			System.out.println("┌────────────────────────────┐\n" + "│  ╔═══╗ Bem-vindo! Welcome! │▒\n"
 					+ "│  ╚═╦═╝ MENU (Nome do jogo) │▒\n" + "╞═╤══╩══╤════════════════════╡▒\n"
@@ -23,35 +33,81 @@ public class Menu {
 			System.out.println("\nDigite uma opção: ");
 			opcao = input.next();
 
-			if (opcao.contentEquals("4") || opcao.equalsIgnoreCase("sair")) {
-				System.out.println("Você saiu.");
-				sair = true;
-			}
-			
-			if (opcao.contentEquals("3") || opcao.equalsIgnoreCase("sobre")) {
-				
-				System.out.println(sinopse);
+			switch (opcao.toLowerCase()) { // deixa toda a opcao lida com caixa baixa
+			case "1":
+			case "jogar":
+				System.out.println(jogar);
 				System.out.println("Voltar? (s/n)");
 				opcaoCondicional = input.next();
-				
-				
-					
-					while (opcaoCondicional.equalsIgnoreCase("n")) {
-						
-					System.out.println(sinopse);
-					System.out.println("Voltar? (s/n)");
-					opcaoCondicional = input.next();
-					
-					
-					};
-					
-				 if (opcaoCondicional.equalsIgnoreCase("s")) {
-					voltar = true;
+
+				if (opcaoCondicional.equalsIgnoreCase("s")) {
+					continue; // retorna para o Menu, o continue traz o próximo laço tradicional sem ser switch-case, no caso o DO-WHILE do menu.
+				} else {
+					do {
+						if (opcaoCondicional.equalsIgnoreCase("s") || opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(jogar);
+						}
+						System.out.println("Voltar? (s/n)");
+						opcaoCondicional = input.next();
+						while (!opcaoCondicional.equalsIgnoreCase("s") && !opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(opcaoInvalida);
+							opcaoCondicional = input.next();
+							continue;
+						}
+					} while (!opcaoCondicional.equalsIgnoreCase("s"));
+					continue; // continue necessário para o término do laço if 
 				}
-				else {
-					System.out.println("Opção inválida!! \n");
-					return;
+
+			case "2":
+			case "opcoes":
+				System.out.println(opcoes);
+				System.out.println("Voltar? (s/n)");
+				opcaoCondicional = input.next();
+
+				if (opcaoCondicional.equalsIgnoreCase("s")) {
+					continue; // retorna ao Menu
+				} else {
+					do {
+						if (opcaoCondicional.equalsIgnoreCase("s") || opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(opcoes);
+						}
+						System.out.println("Voltar? (s/n)");
+						opcaoCondicional = input.next();
+						while (!opcaoCondicional.equalsIgnoreCase("s") && !opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(opcaoInvalida);
+							opcaoCondicional = input.next();
+							continue;
+						}
+					} while (!opcaoCondicional.equalsIgnoreCase("s"));
+					continue;
 				}
+			case "3":
+			case "sobre":
+				System.out.println(sobre);
+				System.out.println("Voltar? (s/n)");
+				opcaoCondicional = input.next();
+
+				if (opcaoCondicional.equalsIgnoreCase("s")) {
+					continue; // retorna ao Menu
+				} else {
+					do {
+						if (opcaoCondicional.equalsIgnoreCase("s") || opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(sobre);
+						}
+						System.out.println("Voltar? (s/n)");
+						opcaoCondicional = input.next();
+						while (!opcaoCondicional.equalsIgnoreCase("s") && !opcaoCondicional.equalsIgnoreCase("n")) {
+							System.out.println(opcaoInvalida);
+							opcaoCondicional = input.next();
+							continue;
+						}
+					} while (!opcaoCondicional.equalsIgnoreCase("s"));
+					continue;
+				}
+			case "4":
+			case "sair":
+				System.out.println("Você saiu do jogo.");
+				sair = true; // novo valor aplicado para a variavel boolean (que faz o laço encerrar)
 			}
 
 		} while (!sair || !voltar);
